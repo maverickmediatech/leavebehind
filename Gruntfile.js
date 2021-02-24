f6bbcbdfe44cb3268c503d3dd712835d06cc8815
Octavi <eoctavi@gmail.com>
2021-02-24 18:24:38 +0100

    removed unnecessary grunt sass task

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function (grunt) {
 
     });
 
-    grunt.loadNpmTasks('grunt-sass');
+
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
